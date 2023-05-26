d9f62291fdf0cba5db8fbbff233a01ecb9658b47
Swohleryz <castellanosantiag026@gmail.com>
2023-05-26 17:55:09 -0300

    resta.js y multiplicacion.js han sido modificados

--- practico0/multiplicacion.js
+++ practico0/multiplicacion.js
@@ -2,7 +2,7 @@ let num1;
 let num2;
 let num3;
 let multiplicacion = num1 * num2 * num3;
-let resultado = "el resultado es " + multiplicacion;
+let resultado = "si a 17 se lo multiplica por 2 y luego por 6, el resultado es " + multiplicacion;
 console.log(num1 = 2);
 console.log(num2 = 17);
 console.log(num3 = 6);
--- practico0/resta.js
+++ practico0/resta.js
@@ -1,7 +1,7 @@
 let minuendo;
 let sustraendo;
 let resta = minuendo - sustraendo;
-let resultado = "el resultado es " + resta;
+let resultado = "si a 10 se le resta 2, el resultado es " + resta;
 console.log(minuendo = 10);
 console.log(sustraendo = 2);
 console.log(resultado);
