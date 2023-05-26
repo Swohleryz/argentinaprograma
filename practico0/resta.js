a8f5ad25d3e4dc77f07569a564203e0479b452b9
Swohleryz <castellanosantiag026@gmail.com>
2023-05-26 18:13:58 -0300

    resta.js y multiplicacion.js han sido editados de nuevo

--- practico0/resta.js
+++ practico0/resta.js
@@ -2,6 +2,6 @@ let minuendo;
 let sustraendo;
 let resta = minuendo - sustraendo;
 let resultado = "si a 10 se le resta 2, el resultado es " + resta;
-console.log(minuendo = 10);
-console.log(sustraendo = 2);
+minuendo = 10;
+sustraendo = 2;
 console.log(resultado);
