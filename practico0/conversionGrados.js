89582edc71144b6089216ac4ccd9843c094ede27
Swohleryz <castellanosantiag026@gmail.com>
2023-05-26 20:45:50 -0300

    revisado y agregado el archivo conversionGrados.js

--- practico0/conversionGrados.js
+++ practico0/conversionGrados.js
@@ -1,9 +1,9 @@
-let gradosFahrenheit;
-let conversion = (gradosFahrenheit - 32) * (5 / 9);
-let gradosCelsius = gradosFahrenheit + " grados F equivalen a " + conversion + " grados C";
+let gradosFahrenheit = 0;
+function conversion(gradosFahrenheit) { return (gradosFahrenheit - 32) * (5 / 9) };
+function gradosCelsius(gradosFahrenheit) { return gradosFahrenheit + " grados F equivalen a " + Math.round(conversion(gradosFahrenheit)) + " grados C" };
 console.log("a. ¿Cuántos grados C son 50 F?");
 gradosFahrenheit = 50;
-console.log(gradosCelsius);
+console.log(gradosCelsius(gradosFahrenheit));
 console.log("b. ¿Cuántos grados C son 75 F?");
 gradosFahrenheit = 75;
-console.log(gradosCelsius);
+console.log(gradosCelsius(gradosFahrenheit));
