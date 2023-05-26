b0c88d0a0de33b3b2001b7af976037102587c4ae
Swohleryz <castellanosantiag026@gmail.com>
2023-05-26 20:00:25 -0300

    revisados y agregados los archivos resta.js y multiplicacion.js nuevamente

--- practico0/multiplicacion.js
+++ practico0/multiplicacion.js
@@ -1,9 +1,9 @@
-let num1;
-let num2;
-let num3;
-let multiplicacion = num1 * num2 * num3;
-let resultado = "si a 17 se lo multiplica por 2 y luego por 6, el resultado es " + multiplicacion;
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+function multiplicacion(num1, num2, num3) { return num1 * num2 * num3 };
+function resultado() { return "si a 17 se lo multiplica por 2 y luego por 6, el resultado es " + multiplicacion(num1, num2, num3) };
 num1 = 2;
 num2 = 17;
 num3 = 6;
-console.log(resultado);
+console.log(resultado());
--- practico0/resta.js
+++ practico0/resta.js
@@ -1,7 +1,7 @@
-let minuendo;
-let sustraendo;
-let resta = minuendo - sustraendo;
-let resultado = "si a 10 se le resta 2, el resultado es " + resta;
+let minuendo = 0;
+let sustraendo = 0;
+function resta(minuendo, sustraendo) { return minuendo - sustraendo };
+function resultado() { return "si a 10 se le resta 2, el resultado es " + resta(minuendo, sustraendo) };
 minuendo = 10;
 sustraendo = 2;
-console.log(resultado);
+console.log(resultado());
