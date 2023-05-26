d5a38f446c7795ebaf6426485b3c0f3ae04e0782
Swohleryz <castellanosantiag026@gmail.com>
2023-05-26 18:15:24 -0300

    conversionGrados.js ha sido agregado

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,9 @@
+let gradosFahrenheit;
+let conversion = (gradosFahrenheit - 32) * (5 / 9);
+let gradosCelsius = gradosFahrenheit + " grados F equivalen a " + conversion + " grados C";
+console.log("a. ¿Cuántos grados C son 50 F?");
+gradosFahrenheit = 50;
+console.log(gradosCelsius);
+console.log("b. ¿Cuántos grados C son 75 F?");
+gradosFahrenheit = 75;
+console.log(gradosCelsius);
