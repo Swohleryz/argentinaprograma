d8352a44fb52132efe93100cb5c427a65e086b64
Swohleryz <castellanosantiag026@gmail.com>
2023-05-26 17:35:01 -0300

    resta.js y multiplicacion.js han sido agregados

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,9 @@
+let num1;
+let num2;
+let num3;
+let multiplicacion = num1 * num2 * num3;
+let resultado = "el resultado es " + multiplicacion;
+console.log(num1 = 2);
+console.log(num2 = 17);
+console.log(num3 = 6);
+console.log(resultado);
