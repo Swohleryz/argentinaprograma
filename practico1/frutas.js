af38866691220c5c212414a9dab5b2124f872922
Swohleryz <castellanosantiag026@gmail.com>
2023-06-10 06:40:54 -0300

    finalizado el quinto ejercicio y también el práctico

---
+++ practico1/frutas.js
@@ -0,0 +1,6 @@
+let frutas = [];
+frutas.push("Banana", "Naranja", "Manzana", "Durazno", "Pera");
+let ultima = frutas[frutas.length - 1];
+console.log('La última fruta agregada fue: %s', ultima);
+let primera = frutas[0];
+console.log('La primer fruta agregada fue: %s', primera);
