60503684df3c08be7aafe20b8ca663d866376cda
Swohleryz <castellanosantiag026@gmail.com>
2023-06-10 05:27:15 -0300

    finalizado el primer ejercicio del práctico

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,5 @@
+let nombre = "";       // Guardo un valor en la variable en dos pasos, es solo... \\
+nombre = "Santiago";     // ...para distinguir entre los puntos a. y b. en la consigna \\
+let apellido = "Castellanos";
+let edad = 29;
+console.log('Hola, %s %s. Tienes %i años.', nombre, apellido, edad);
